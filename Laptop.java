ed8e89bbbb4facd9ad145245b07709352af83b09
PalomaAhhh <paulina.sommer.ps@gmail.com>
2026-09-24 00:07:29 +0200

    Initial sharing of project

---
+++ Laptop.java
@@ -0,0 +1,14 @@
+public class Laptop
+{
+    
+    private String hersteller;
+    private int kosten;
+    private boolean neu;
+
+    public Laptop()
+    {
+        hersteller = "Lenovo";
+        kosten = 1000;
+        neu = true;
+    }
+}
